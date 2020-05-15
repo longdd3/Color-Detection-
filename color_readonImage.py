7abbf76ffdd895ffe61fbae48c045a5e3714b1ca
Dang Long <43181516+longdd3@users.noreply.github.com>
2020-05-15 14:47:34 -0500

    Update and rename color_change.py to color_readonImage.py

--- color_readonImage.py
+++ color_readonImage.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def nothing(x):
     pass
-
+cap = cv2.VideoCapture(0);
 cv2.namedWindow("Tracking")
 cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
 cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
@@ -13,8 +13,8 @@ cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
 cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
 while True:
-    frame = cv2.imread('color.png')
-
+    _,frame = cap.read()
+    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     l_h = cv2.getTrackbarPos("LH", "Tracking")
@@ -39,5 +39,5 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break
-
+cap.release()
 cv2.destroyAllWindows()
